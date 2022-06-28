c3542865c87b4f873728697142f5ec5c9c849821
Dorian Edwards <de.pharmd2018@gmail.com>
2022-06-27 20:11:56 -0400

    add solution to 1.1

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,34 @@
+const Header = (props) => <h1>Course: {props.course}</h1>
+const Content = (props) => (
+  <p>
+    {props.part}: {props.exercises}
+  </p>
+)
+
+const Total = (props) => {
+  const total = props.total.reduce((a, b) => a + b, 0)
+
+  return <div>Number of exercises: {total}</div>
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Total total={[exercises1, exercises2, exercises3]} />
+    </div>
+  )
+}
+
+export default App

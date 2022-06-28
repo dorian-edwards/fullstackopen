417339b1f3abe9ba50a1284a87508637ac45ac46
Dorian Edwards <de.pharmd2018@gmail.com>
2022-06-28 00:49:42 -0400

    add solution to exercise 1.4

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -22,27 +22,26 @@ const Total = ({ data }) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10,
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7,
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14,
-  }
-
-  const dataSet = [part1, part2, part3]
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10,
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7,
+    },
+    {
+      name: 'State of a component',
+      exercises: 14,
+    },
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content data={dataSet} />
-      <Total data={dataSet} />
+      <Content data={parts} />
+      <Total data={parts} />
     </div>
   )
 }

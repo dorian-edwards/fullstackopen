0dbe688bd0ff6d07597784bfd86694a799c47926
Dorian Edwards <de.pharmd2018@gmail.com>
2022-07-01 09:57:49 -0400

    add solution to 2.6

---
+++ part2/phoebook/src/App.js
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [newName, setNewName] = useState('name')
+
+  const handleInput = (e) => {
+    setNewName(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const newPerson = { name: newName }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input value={newName} onChange={handleInput} />
+        </div>
+        <div>
+          <button type='submit'>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((obj, i) => (
+        <div key={obj.name}>{obj.name}</div>
+      ))}
+    </div>
+  )
+}
+
+export default App

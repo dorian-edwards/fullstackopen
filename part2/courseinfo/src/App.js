1ef854e3004753e2d93748b643fd65712a24d81e
Dorian Edwards <de.pharmd2018@gmail.com>
2022-07-01 08:13:32 -0400

    refactor for exercise 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,37 +1,4 @@
-const Header = ({ name }) => <h2>{name}</h2>
-
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-)
-
-const Total = ({ parts }) => {
-  const sum = parts.reduce((total, obj) => {
-    total += obj.exercises
-    return total
-  }, 0)
-
-  return <strong>total of {sum} exercises</strong>
-}
-
-const Content = ({ course }) => (
-  <>
-    {course.parts.map((part) => (
-      <Part part={part} key={part.id} />
-    ))}
-    <Total parts={course.parts} />
-  </>
-)
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header name={course.name} />
-      <Content course={course} />
-    </>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [

77c7ada0a69aa6ebac6f5d7995d99fe286919a1d
Dorian Edwards <de.pharmd2018@gmail.com>
2022-07-01 21:27:31 -0400

    add solution to exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,15 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '123-456-7890' },
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
   ])
+
   const [newName, setNewName] = useState('name')
   const [newNumber, setNewNumber] = useState('000-000-0000')
+  const [filter, setFilter] = useState('')
 
   const handleName = (e) => setNewName(e.target.value)
 
@@ -25,9 +30,21 @@ const App = () => {
     setNewNumber('')
   }
 
+  const handleFilter = (e) => setFilter(e.target.value)
+
+  const eligibleEntries = filter
+    ? persons.filter(({ name }) =>
+        name.toLowerCase().includes(filter.toLowerCase())
+      )
+    : persons
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Show entries containing:
+        <input value={filter} onChange={handleFilter} />
+      </div>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={handleName} />
@@ -40,7 +57,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((obj, i) => (
+      {eligibleEntries.map((obj, i) => (
         <div key={obj.name}>
           {obj.name} {obj.number}
         </div>

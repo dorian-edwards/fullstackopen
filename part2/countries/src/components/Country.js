55a6603fdc7a0ed4f0a465219bd2f4f3be28e52b
Dorian Edwards <de.pharmd2018@gmail.com>
2022-07-04 01:17:11 -0400

    add solution for exercise 2.14

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,17 +1,54 @@
-const Country = ({ country }) => (
-  <>
-    <h2>{country.name.common}</h2>
-    <div>capital {country.capital[0]}</div>
-    <div>area {country.area}</div>
-    <br />
-    <strong>languages</strong>
-    <ul>
-      {Object.values(country.languages).map((language) => (
-        <li key={language}>{language}</li>
-      ))}
-    </ul>
-    <img src={country.flags.png} alt={`flag of ${country.name.common}`}></img>
-  </>
-)
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Country = ({ country }) => {
+  const [weather, setWeather] = useState(null)
+
+  useEffect(() => {
+    const [lat, lon] = country.latlng
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}`
+      )
+      .then(({ data }) => {
+        setWeather({
+          temp: data.main.temp,
+          icon: `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
+          alt: data.weather[0].description,
+          wind: data.wind.speed,
+        })
+      })
+  }, [])
+
+  return (
+    <>
+      <h2>{country.name.common}</h2>
+      <div>capital {country.capital[0]}</div>
+      <div>area {country.area}</div>
+      <br />
+      <strong>languages</strong>
+      <ul>
+        {Object.values(country.languages).map((language) => (
+          <li key={language}>{language}</li>
+        ))}
+      </ul>
+      <img src={country.flags.png} alt={`flag of ${country.name.common}`}></img>
+      <br />
+      <h2>Weather in {country.capital}</h2>
+      {weather ? (
+        <>
+          <p>temperature {weather.temp} Celcius</p>
+          <img
+            src={weather.icon}
+            alt={`icon representing ${weather.alt}`}
+          ></img>
+          <p>wind {weather.wind} m/s</p>
+        </>
+      ) : (
+        <div>loading ...</div>
+      )}
+    </>
+  )
+}
 
 export default Country
